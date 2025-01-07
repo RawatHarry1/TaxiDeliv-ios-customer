d2d11ef220c13ef08a48356f6918b28d61b7daf1
paramTechbuilder9712 <paramveer@techbuilder.ai>
2025-01-07 19:07:38 +0530

    Bug Fixes and VCPackageDetailVC added

---
+++ VenusCustomer/Modules/PostLogin/Package Detail VC/PackageDetailViewTblCell.swift
@@ -0,0 +1,23 @@
+//
+//  PackageDetailViewTblCell.swift
+//  VenusCustomer
+//
+//  Created by TechBuilder on 07/01/25.
+//
+
+import UIKit
+
+class PackageDetailViewTblCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
